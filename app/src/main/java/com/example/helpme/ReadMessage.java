505e2bc9f7f2738298d738b346ce6001adee5ba9
Unzilla <unzilla.a@yahoo.com>
2020-08-28 12:35:23 +0500

    SecondReadMessage

---
+++ app/src/main/java/com/example/helpme/ReadMessage.java
@@ -0,0 +1,14 @@
+package com.example.helpme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReadMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_message);
+    }
+}
