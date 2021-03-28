73c80b02cf853183bad77d756c2e8f9fc68848a7
Unzilla <57434325+Unzilla@users.noreply.github.com>
2021-03-29 00:08:45 +0500

    Update Music.java

--- app/src/main/java/com/example/helpme/Music.java
+++ app/src/main/java/com/example/helpme/Music.java
@@ -1,5 +1,4 @@
 package com.example.helpme;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -19,10 +18,8 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
